0a2585b9702352b94657a1164a043ed50b49f84d
elchudi <gustavogiudici@gmail.com>
2015-03-16 09:07:18 -0300

    using new module npm connector

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 // DEPENDENCIES
 var bunyan = require('bunyan');
-var log = bunyan.createLogger({name: "chaski"});
-
 if(!process.argv[2]){
     log.info('Must give atahualpa ip');
     process.exit(9);
 }
 
 var IP_ATAHUALPA = process.argv[2];
+var chaski_connector = require('chaski-connector')({ipAtahualpa:IP_ATAHUALPA});
+var log = bunyan.createLogger({name: "chaski"});
+
 
 var clientPings = require('./worker/clientPings')();
 var messagePublisher = require('./worker/messagePublisher')();
-var messageReceiver = require('./worker/messageReceiver')({"meesagePublisher": messagePublisher, "atahualpas": [{"ip": IP_ATAHUALPA}]});
-var channelAssign = require('./worker/channelAssign')({"messageReceiver": messageReceiver});
+var messageReceiver = chaski_connector.messageReceiver({"meesagePublisher": messagePublisher, "atahualpas": [{"ip": IP_ATAHUALPA}]});
+var channelAssign = chaski_connector.channelAssign({"messageReceiver": messageReceiver});
 
