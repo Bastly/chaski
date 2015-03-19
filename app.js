4aff9c7c3cc0c098bfc8baa5f3cada47efdd92b2
elchudi <gustavogiudici@gmail.com>
2015-03-19 13:07:32 -0300

    adds new chaki-connect required chaskiType param

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 // DEPENDENCIES
 var bunyan = require('bunyan');
+var constants = require('bastly_constants');
 if(!process.argv[2]){
     log.info('Must give atahualpa ip');
     process.exit(9);
 }
 
 var IP_ATAHUALPA = process.argv[2];
-var chaski_connector = require('chaski-connector')({ipAtahualpa:IP_ATAHUALPA});
+var chaski_connector = require('chaski-connector')({ipAtahualpa:IP_ATAHUALPA, typeChaski:constants.CHASI_TYPE_ZEROMQ});
 var log = bunyan.createLogger({name: "chaski"});
 
 
